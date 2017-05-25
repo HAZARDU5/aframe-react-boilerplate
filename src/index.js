d6e46d1398b700dbc9a247b94722cc5481b9b931
Michael Andrew <michael@gladeye.co.nz>
2017-05-25 15:20:15 +1200

    Moved scss and enabled hot loader integration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
+require("react-hot-loader/patch") //must come first
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App'
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import { AppContainer } from 'react-hot-loader'
+
+const render = Component => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        document.getElementById('root')
+    )
+};
+
+render(App);
+
+if (module.hot) {
+    module.hot.accept('./scss/main.scss', () => {
+        require('./scss/main.scss');
+    });
+    module.hot.accept('./components/App', () => { render(App) })
+}
