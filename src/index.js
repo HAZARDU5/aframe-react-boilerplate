a37535f8a0cf346ae3787c388660c4773c1f8997
Michael Andrew <michael@gladeye.co.nz>
2017-05-26 13:32:39 +1200

    Updated CSS path for live reload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-require("react-hot-loader/patch"); //must come first
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { AppContainer } from 'react-hot-loader'
+import './css/main.css';
 
 const render = Component => {
     ReactDOM.render(
@@ -15,15 +14,3 @@ const render = Component => {
 };
 
 render(App);
-
-if (module.hot) {
-    console.log('*** Hot reloading active ***');
-    module.hot.accept('./scss/main.scss', () => {
-        console.log('*** Hot reloading CSS now ***');
-        require('./scss/main.scss');
-    });
-    module.hot.accept('./components/App', () => {
-        console.log('*** Hot reloading app now ***');
-        render(App)
-    })
-}
