16548c5cefe0255333750d5559e7bcbc38639a76
Michael Andrew <michael@gladeye.co.nz>
2017-05-26 13:58:44 +1200

    Removed hot loader requirement
    `react-scripts` already uses live reload - so let’s just stick with that.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { AppContainer } from 'react-hot-loader'
 import './css/main.css';
 
 const render = Component => {
     ReactDOM.render(
-        <AppContainer>
-            <Component />
-        </AppContainer>,
+        <Component />,
         document.getElementById('root')
     )
 };
